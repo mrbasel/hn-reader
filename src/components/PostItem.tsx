64b410824a0bbce97e2101d4948c4f6dbf12b701
mrbasel <mrbasel555@gmail.com>
2021-08-03 23:25:08 +0300

    small changes

--- src/components/PostItem.tsx
+++ src/components/PostItem.tsx
@@ -1,9 +1,9 @@
-import { Box, Flex, Heading, HStack, Image, Text } from "@chakra-ui/react";
+import { Box, Flex, Heading, HStack, Text } from "@chakra-ui/react";
 
 function PostItem(props: any) {
   return (
     <Flex my="4" p="3" border="1px solid #333">
-      <Image src="https://via.placeholder.com/100" />
+      <Heading>{props.index}</Heading>
       <Flex
         flexDir="column"
         justifyContent="space-between"
--- src/components/PostsList.tsx
+++ src/components/PostsList.tsx
@@ -3,11 +3,12 @@ import PostItem from "./PostItem";
 
 function PostsList(props: any) {
   return (
-    <Box maxW="960px" mt="8" p={4} color="white">
+    <Box maxW="960px" mx="auto" mt="8" p={4} color="white">
       {[...Array(3)].map((e, i) => (
         <PostItem
+          index={i + 1}
           title="ac placerat vestibulum lectus mauris ultrices eros in cursus turpis"
-          author={`internet_guy_${i}`}
+          author={`internet_guy_${i + 1}`}
           points={20}
           comments={4}
         />
