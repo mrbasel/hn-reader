221ed6987eea2256f6bf9c57e761312fab4ff9b7
mrbasel <mrbasel555@gmail.com>
2021-09-23 22:21:06 +0300

    add explanation for 'ScrollToTop' component

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -1,7 +1,12 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
-// Scrolls to top of the page on any page change
+// Scrolls to top of the page when navigating to a "/post" page
+// ----- //
+// When using chrome on mobile, sometimes after navigating
+// to a post you end up in the middle of the page
+// instead of being at the top, which is what this
+// component is used for
 function ScrollToTop() {
   const { pathname } = useLocation();
 
