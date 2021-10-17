3e91424a06366d9be20a83f08ea633b73b5f378d
mrbasel <mrbasel555@gmail.com>
2021-10-17 13:21:59 +0300

    change nav layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,40 +4,62 @@ import {
   Box,
   Link,
   useMediaQuery,
+  Icon,
+  Button,
 } from "@chakra-ui/react";
 import { Link as RouterLink } from "react-router-dom";
+import { FiSettings } from "react-icons/fi";
+import { HiMenu } from "react-icons/hi";
 
 function Header() {
-  const [isSmallerThan500] = useMediaQuery("(max-width: 500px)");
+  const [isSmallerThan800] = useMediaQuery("(max-width: 800px)");
   return (
-    <Flex
-      as="nav"
-      justifyContent="space-around"
-      alignItems="center"
-      pb="3"
-      mt="2"
-      borderBottom="1px solid orange"
-    >
-      <Heading>
-        <Link as={RouterLink} to="/top">
-          {isSmallerThan500 ? "HN" : "Hacker News"}
-        </Link>
-      </Heading>
-      <Box>
-        <Link as={RouterLink} to="/ask" mx="4">
-          Ask
-        </Link>
-        <Link as={RouterLink} to="/show" mx="4">
-          Show
-        </Link>
-        <Link as={RouterLink} to="/newest" mx="4">
-          New
-        </Link>
-        <Link as={RouterLink} to="/about" mx="4">
-          About
-        </Link>
-      </Box>
-    </Flex>
+    <Box as="nav" borderBottom="1px solid orange">
+      <Flex
+        position="relative"
+        justifyContent="center"
+        alignItems="center"
+        pb="3"
+        mt="2"
+        maxW="1500px"
+        mx="auto"
+      >
+        <Box position="absolute" left="3">
+          {isSmallerThan800 ? (
+            <Icon as={HiMenu} fontSize="3xl" />
+          ) : (
+            <Box>
+              <Link as={RouterLink} to="/ask" mx="4">
+                Ask
+              </Link>
+              <Link as={RouterLink} to="/show" mx="4">
+                Show
+              </Link>
+              <Link as={RouterLink} to="/newest" mx="4">
+                New
+              </Link>
+              <Link as={RouterLink} to="/newest" mx="4">
+                Jobs
+              </Link>
+            </Box>
+          )}
+        </Box>
+
+        <Heading>
+          <Link as={RouterLink} to="/top">
+            {isSmallerThan800 ? "HN" : "HN News"}
+          </Link>
+        </Heading>
+        <Box position="absolute" right="3">
+          <Link as={RouterLink} to="/about" mx="4">
+            About
+          </Link>
+          <Link as={RouterLink} to="/settings" mx="4">
+            <Button>Settings</Button>
+          </Link>
+        </Box>
+      </Flex>
+    </Box>
   );
 }
 
