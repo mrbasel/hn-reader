a6ade1c338275c845cb4a6d3c61ac09944d26631
mrbasel <mrbasel555@gmail.com>
2021-11-04 21:12:26 +0300

    update about page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,22 +3,26 @@ import { useEffect } from "react";
 
 function About() {
   useEffect(() => {
-    document.title = "About - HN"
+    document.title = "About - HN";
   }, []);
 
   return (
-    <Box mt="8" textAlign="center">
+    <Box my="8" p="4" textAlign="left">
       <Heading>About</Heading>
-      <Box mt="4" fontSize="lg">
+      <Box mt="4">
         <Text>
           This is a simple Hacker News reader app with a mobile friendly UI and
           dark mode support.
         </Text>
-        <Text>Made using TypeScript, React.js and Chakra UI.</Text>
+        <Text my="2">Made using TypeScript, React.js and Chakra UI.</Text>
         <Text>
-          <Link color="#4fbcff" href="https://github.com/mrbasel/hn-reader">
-            Source Code
-          </Link>
+          <Text>
+            If you found this app helpful, please consider leaving a star on the{" "}
+            <Link color="#4fbcff" href="https://github.com/mrbasel/hn-reader">
+              Github repo{" "}
+            </Link>{" "}
+            😊
+          </Text>
         </Text>
       </Box>
     </Box>
